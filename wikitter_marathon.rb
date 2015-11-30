ff168494b4b80481cf04db084d8a2e7cbe5d4a38
saxsir <saxsir.256+github@gmail.com>
2015-12-01 00:37:30 +0900

    短いページから試していくように改良

--- wikitter_marathon.rb
+++ wikitter_marathon.rb
@@ -20,9 +20,37 @@ def calc_dist(word)
   return -1
 end
 
+def fetch_short_pages
+  html_string = `curl -sS https://ja.wikipedia.org/wiki/Wikipedia:%E7%9F%AD%E3%81%84%E3%83%9A%E3%83%BC%E3%82%B8`
+  if result = Nokogiri::HTML(html_string).css('#mw-content-text > ol > li > a')
+    return result.map {|a| a.content }
+  end
+  return []
+end
+
+def fetch_special_short_pages(limit, offset)
+  html_string = `curl -sS https://ja.wikipedia.org/w/index.php\\?title=特別:短いページ\\&limit=#{limit}\\&offset=#{offset}`
+  if result = Nokogiri::HTML(html_string).css('#mw-content-text > div > ol > li > a:nth-child(2)')
+    return result.map {|a| a.content }
+  end
+  return []
+end
+
 dist = 0
+offset = 0
+short_pages = fetch_short_pages()
+
 until dist >= 7 do
-  w = fetch_random_keyword()
+  if short_pages.empty?
+    limit = 100
+    short_pages = fetch_special_short_pages(limit, offset)
+    offset += limit
+  end
+
+  unless w = short_pages.shift()
+    w = fetch_random_keyword()
+  end
+
   d = calc_dist(w).to_i
   puts "#{w}: #{d}"
   dist = d
